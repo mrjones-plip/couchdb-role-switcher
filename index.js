408b573b386568c0f30713b51703bc60817d6508
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2022-09-12 10:00:10 +1200

    Refactorizationalism

--- index.js
+++ index.js
@@ -1,121 +1,74 @@
-'use strict';
 const request = require('request-promise-native');
 
 const role_to_use = process.env.ROLE;
 const couch_url = process.env.COUCH_URL;
-let compiledUrl = {};
 
-if(role_to_use === undefined){
+if(role_to_use === undefined) {
     console.log('ROLE is not set, please set it and try again');
     process.exit(1);
 }
-if(couch_url === undefined){
+if(couch_url === undefined) {
     console.log('COUCH_URL is not set, please set it and try again');
     process.exit(1);
 }
 
-const handleError = function(e){
-    if (e.statusCode === 401) {
-        console.log('Bad authentication for CouchDB. Check that COUCH_URL has correct usernames and passwords.');
-        process.exit(1);
-    } else {
-        console.log('Error connecting to CouchDB: ' + e.message);
-        process.exit(1);
-    }
-}
-
-const getAllUsers = function (){
+const compileUrl = (path) => {
     try {
-        compiledUrl = new URL('/_users/_all_docs', couch_url);
+        return new URL(path, couch_url).href;
     } catch(e) {
-        console.log('Error while creating url: ', e.message);
-        process.exit(1);
+        throw new Error(`Error while creating url: ${e.message}`);
     }
+};
 
-    const options = {
-        uri: compiledUrl.href,
-        json: true
-    };
-
-    return request.get(options)
-        .then(users => {
-            if (typeof users.rows === 'object' && users.rows.length > 0){
-                // first user is always admin user, let's not mess with them
-                users.rows.shift();
-                console.log("Found",users.rows.length,"total users.");
-                return users.rows;
-            } else {
-                console.log('No users found');
-                process.exit(1);
-            }
-        })
-        .catch(e => {
-            handleError(e);
-        })
+const getAllUsers = async () => {
+    const uri = compileUrl('/_users/_all_docs');
+    return request.get({ uri, json: true});
 }
 
-const getUserPerms = function (allUsers){
-    allUsers.forEach((username)=>{
-        const userId = username.id;
-        const usernameAry = userId.split(':');
-        try {
-            compiledUrl = new URL('/_users/' + userId, couch_url);
-        } catch(e) {
-            console.log('Error while creating url: ', e.message);
-            process.exit(1);
+const filterOutAdmins = (users) => {
+    if (typeof users.rows === 'object' && users.rows.length > 1){
+        // first user is always admin user, let's not mess with them
+        users.rows.shift();
+        console.log(`Found ${users.rows.length} total users.`);
+        return users.rows;
+    }
+    return [];
+};
+
+const getUpdatedUsers = async (allUsers) => {
+    const updates = [];
+    for (const user of allUsers) {
+        const uri = compileUrl('/_users/' + user.id);
+        const user = await request.get({ uri, json: true });
+        if (typeof user.roles === 'object' && !user.roles.includes(role_to_use)) {
+            user.roles.push(role_to_use);
+            updates.push(users);
         }
-
-        const options = {
-            uri: compiledUrl.href,
-            json: true
-        };
-
-        return request.get(options)
-            .then(userData => {
-                if (typeof userData.roles === 'object'){
-                    if (!userData.roles.includes(role_to_use)){
-                        userData.roles.push(role_to_use);
-                        setPermission(userId, {
-                                'roles': userData.roles,
-                                '_rev': userData._rev,
-                                'type': "user",
-                                'name': usernameAry[1],
-                            }
-                        );
-                    }
-                }
-            })
-            .catch(e => {
-                handleError(e);
-            });
-    })
-}
-
-const setPermission = function(userId, userRoles){
-    let compiledUrl
-    let options;
-    try {
-        compiledUrl = new URL('/_users/' + userId, couch_url);
-    } catch(e) {
-        console.log('Error while creating url:', e.message);
-        process.exit(1);
     }
-    options = {
-        uri: compiledUrl.href,
-        json: true,
-        body: userRoles,
-    };
-
-    return request.put(options)
-        .then(() => {
-            console.log('set success:', userRoles.name);
-        })
-        .catch(e => {
-            console.log('set FAIL:', userId, 'for roles:', userRoles, ' at URL ', compiledUrl.href);
-            handleError(e);
-        });
+    return updates;
 }
 
-console.log("\nStarting, will try and add role:", role_to_use, "against CouchDB instance at:", couch_url,"\n")
-getAllUsers()
-    .then((allUsers) => getUserPerms(allUsers));
+const saveUsers = async (users) => {
+    for (const user of users) {
+        const userId = user._id.split(':')[1];
+        const uri = compileUrl('/_users/' + userId);
+        await request.put({ uri, json: true, body: user });
+    }
+}
+
+const go = async () => {
+    try {
+        const allUsers = await getAllUsers();
+        const nonAdmins = await filterOutAdmins(allUsers);
+        const updatedUsers = await getUpdatedUsers(nonAdmins);
+        await saveUsers(updatedUsers);
+    } catch (e) {
+        if (e.statusCode === 401) {
+            console.log('Bad authentication for CouchDB. Check that COUCH_URL has correct usernames and passwords.');
+        } else {
+            console.log('Error connecting to CouchDB: ' + e.message);
+        }
+    }
+};
+
+go();
